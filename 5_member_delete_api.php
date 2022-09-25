39228742b5877fa521d914538d55867f89c31888
Kunda <love60066@gmail.com>
2022-09-25 18:06:42 +0800

    member_delete_api增加獎勵刪除條件

--- 5_member_delete_api.php
+++ 5_member_delete_api.php
@@ -2,11 +2,15 @@
 
 $sid = isset($_GET['sid']) ? intval($_GET['sid']) : 0;
 
-$sql = "DELETE md.*,cd.*,ad.* FROM `members_data` md
+$sql = "DELETE md.*,cd.*,ad.* ,bd.*,rd.* FROM `members_data` md
   JOIN `contact_data` cd
   ON md.`sid`=cd.`sid`
   JOIN `address_data` ad 
   ON md.`sid`=ad.`sid`
+  JOIN `birth_award_data` bd 
+  ON md.`sid`=bd.`sid`
+  JOIN `register_award_data` rd 
+  ON md.`sid`=rd.`sid`
   WHERE md.`sid`={$sid}";
 
 $pdo->query($sql);
